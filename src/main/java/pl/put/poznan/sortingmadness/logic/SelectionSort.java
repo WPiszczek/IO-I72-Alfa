6e62157167c2b289239a666a50b7ebfc2ecaba4b
martynajedrzejczyk <77231019+martynajedrzejczyk@users.noreply.github.com>
2021-12-11 20:32:38 +0100

    implementing_SelectionSort

--- src/main/java/pl/put/poznan/sortingmadness/logic/SelectionSort.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/SelectionSort.java
@@ -1,14 +1,44 @@
 package pl.put.poznan.sortingmadness.logic;
 
+/**
+ * SelectionSort class
+ */
 public class SelectionSort extends SortingMadness {
 
+    /**
+     * SelectionSort class constructor
+     * @param array array of type Object
+     */
     public SelectionSort(Object[] array) {
         super(array);
     }
 
+    /**
+     * Sorting method
+     * @return array of type Object
+     */
     @Override
     public Object[] sort() {
-        return this.array;
+        Object[] array = this.array.clone();
+
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            Object min = array[i];
+            int minimIndex = i;
+            for (int j = i+1; j < array.length; j++) {
+                Comparable a = (Comparable) array[j];
+                // array[j] < min
+                if (a.compareTo(min) < 0) {
+                    min = array[j];
+                    minimIndex = j;
+                }
+            }
+
+            Object temp = array[i];
+            array[i] = min;
+            array[minimIndex] = temp;
+        }
+        return array;
     }
 }
 
