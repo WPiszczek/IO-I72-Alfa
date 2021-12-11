8b091741af68a5c6fa1f3d6a282f979703dc6e9d
Akaczek <66081037+Akaczek@users.noreply.github.com>
2021-12-11 20:32:08 +0100

    implementing InsertionSort

--- src/main/java/pl/put/poznan/sortingmadness/logic/InsertionSort.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/InsertionSort.java
@@ -3,7 +3,46 @@ package pl.put.poznan.sortingmadness.logic;
 public class InsertionSort extends SortingMadness {
 
     public InsertionSort(Object[] array) {
+        super(array);package pl.put.poznan.sortingmadness.logic;
+
+/**
+ * InsertionSort class
+ */
+public class InsertionSort extends SortingMadness {
+
+    /**
+     * InsertionSort class constructor
+     * @param array array of type Object
+     */
+    public InsertionSort(Object[] array) {
         super(array);
+    }
+
+    /**
+     * Sorting method
+     * @return array of type Object
+     */
+    @Override
+    public Object[] sort() {
+        Object[] array = this.array.clone();
+        int n = array.length;
+
+        for (int i = 1; i < n; ++i) {
+            Object key = array[i];
+            int j = i - 1;
+
+            Comparable a = (Comparable) key;
+            while (j >= 0 && a.compareTo(array[j]) < 0) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+        return array;
+    }
+}
+
+
     }
 
     @Override
