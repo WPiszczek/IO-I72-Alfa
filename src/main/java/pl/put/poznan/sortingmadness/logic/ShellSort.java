0b78494ea482a560b5182c40078915ac2261c531
Akaczek <66081037+Akaczek@users.noreply.github.com>
2021-12-14 22:58:05 +0100

    implementing ShellSort

--- src/main/java/pl/put/poznan/sortingmadness/logic/ShellSort.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/ShellSort.java
@@ -8,7 +8,23 @@ public class ShellSort extends SortingMadness {
 
     @Override
     public Object[] sort() {
-        return this.array;
+        Object[] array = this.array.clone();
+        int n = array.length;
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                Object key = array[i];
+                int j = i;
+                Comparable a = (Comparable) key;
+                while (j >= gap && a.compareTo(array[j - gap]) < 0) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+                array[j] = key;
+            }
+        }
+
+        return array;
     }
 }
 
