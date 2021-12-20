42d70ff92f7f9ffadfae8fe97ee0895b4079df00
martynajedrzejczyk <martyna.jedrzejczyk@student.put.poznan.pl>
2021-12-20 13:56:04 +0100

    edit of documentation

--- src/main/java/pl/put/poznan/sortingmadness/app/SortingMadnessApplication.java
+++ src/main/java/pl/put/poznan/sortingmadness/app/SortingMadnessApplication.java
@@ -13,7 +13,6 @@ public class SortingMadnessApplication {
 
     /**
      * Main function
-     * @param args
      */
     public static void main(String[] args) {
         SpringApplication.run(SortingMadnessApplication.class, args);
